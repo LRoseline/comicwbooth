52573329cb2c764e03932e60778f1b99dbf4d82b
LRoseline <amb17@naver.com>
2024-06-20 23:08:20 +0900

    load

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
                 console.log(error);
                 setLoading(false);
             });
-    }, [header]);
+    }, []);
 
     const openDetail = (product) => {
         setSelectedProduct(product);
